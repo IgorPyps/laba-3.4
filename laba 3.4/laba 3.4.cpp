5bea7c364e5b1463c22af9d2991e19efdffb7429
Igor <igorvlasenko97@gmail.com>
2022-10-27 21:43:23 +0300

    laba3.4

---
+++ laba 3.4/laba 3.4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+	double y; // вхідний параметр
+	double R; // радіус
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	cout << "R = "; cin >> R;
+
+	// розгалуження в повній формі
+	if (y >= -R && y <= R && x <= R && x >= -R && (x + R) * (x + R) + (y - R) * (y - R) >= R * R && (x - R) * (x - R) + (y + R) * (y + R) >= R * R)
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	cin.get();
+	return 0;
+}
